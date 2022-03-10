0d827cbbea85581833085803904c93d872e9585c
d34db4b3 <thomas.potier@outlook.com>
2022-03-10 15:50:38 +0100

    Analyse d'une route

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{get, web, App, HttpServer};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+struct GreetQuery {
+    name: String,
+}
+
+#[derive(Serialize)]
+struct GreetResponse {
+    greeting: String,
+}
+
+#[get("/greet")]
+async fn greet(greet_query: web::Query<GreetQuery>) -> web::Json<GreetResponse> {
+    web::Json(GreetResponse {
+        greeting: format!("Hello, {}!", greet_query.name),
+    })
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(|| async { "Hello, world!" })))
+    HttpServer::new(|| App::new().service(greet))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
